ff40d80431cc27853b6716dee3d3349f4007f0ff
Ramdhan Mahfuzh <ramdhan.mahfuzh@mhs.itenas.ac.id>
2023-05-23 19:42:09 +0700

    upload header

---
+++ layouts/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="style/fontawesome/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="sticky sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="#">Pojok Buku</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center mx-auto ps-5 " id="navbarNav">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item ps-5">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item ps-5">
+                            <a class="nav-link active" aria-current="page" href="#">Buy</a>
+                        </li>
+                        <li class="nav-item ps-5">
+                            <a class="nav-link active" aria-current="page" href="#">Borrow</a>
+                        </li>
+                        <li class="nav-item ps-5">
+                            <a class="nav-link active" aria-current="page" href="#">About Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="collapse navbar-collapse justify-content-end">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <div class="dropdown  me-5">
+                                <a class="nav-link active" type="button" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page" href="#"><i class="fa-solid fa-user"></i></a>
+                                <ul class="dropdown-menu dropdown-menu-light justify-content-center">
+                                    <li><a class="dropdown-item " href="#">Profile</a></li>
+                                    <li><a class="dropdown-item " href="#">Transaction</a></li>
+                                    <li><a class="dropdown-item  " href="#">Settings</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item text-danger" href="#">Logout</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="py-5">
