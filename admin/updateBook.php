8b655e1c98d2a4dccf27559b6ddf9475323e40fb
azildest <azildest@users.noreply.github.com>
2023-06-01 12:20:11 +0700

    action update book

--- admin/updateBook.php
+++ admin/updateBook.php
@@ -1,3 +1,12 @@
+<?php
+include ('../../server/connection.php');
+session_start();
+
+$id_buku = $_GET['id_buku'];
+
+$add = "SELECT * FROM buku WHERE id_buku = '$id_buku'";
+$query = mysqli_query($conn, $add);
+?>
 
 
 <!DOCTYPE html>
@@ -15,59 +24,59 @@
     <div class="formbold-main-wrapper">
         <div class="formbold-form-wrapper">
             <h3 class="tittle">Update Buku</h3>
-          <form class="mt-5" action="actionAddBook.php" method="POST">
+            <?php while ($row = mysqli_fetch_assoc($query)) : ?>
+          <form class="mt-5" action="actionUpdateBook.php" method="POST">
               <div class="formbold-input-flex-judul">
                 <div>
                     <label for="judul_buku" class="formbold-form-label"> Judul Buku </label>
-                    <input type="text" name="judul_buku" placeholder="Harry Potter" class="formbold-form-input"/>
+                    <input type="text" name="judul_buku" class="formbold-form-input" value="<?php echo $row['judul_buku'] ?>"/>
                 </div>
               </div>
 
               <div class="formbold-input-flex-judul">
                 <div>
                     <label for="penulis_buku" class="formbold-form-label"> Penulis </label>
-                    <input type="text" name="penulis_buku" placeholder="J.K. Rowling" class="formbold-form-input"/>
+                    <input type="text" name="penulis_buku" class="formbold-form-input" value="<?php echo $row['penulis_buku'] ?>"/>
                 </div>
               </div>
       
               <div class="formbold-input-flex">
                 <div>
                     <label for="penerbit" class="formbold-form-label"> Penerbit </label>
-                    <input type="text" name="penerbit_buku" placeholder="Gramedia" class="formbold-form-input"/>
+                    <input type="text" name="penerbit_buku" class="formbold-form-input" value="<?php echo $row['penerbit_buku'] ?>"/>
                 </div>
                 <div>
                     <label for="tahun_terbit" class="formbold-form-label"> Tahun Terbit </label>
-                    <input type="text" name="tahun_terbit" placeholder="1997" class="formbold-form-input"/>
+                    <input type="text" name="tahun_terbit" class="formbold-form-input" value="<?php echo $row['tahun_terbit'] ?>"/>
                 </div>
               </div>
 
               <div class="formbold-input-flex">
                 <div>
                     <label for="harga" class="formbold-form-label"> Harga </label>
-                    <input type="text" name="harga" placeholder="Rp. 60.000" class="formbold-form-input"/>
+                    <input type="text" name="harga" class="formbold-form-input" value="<?php echo $row['harga'] ?>"/>
                 </div>
                 <div>
                     <label for="phone" class="formbold-form-label-stok"> Stok </label>
-                    <input type="number" step="1" min="0" max="99" name="stok" placeholder="0" class="formbold-form-input"/>
+                    <input type="number" step="1" min="0" max="99" name="stok" class="formbold-form-input" value="<?php echo $row['stok'] ?>"/>
                 </div>
               </div>
 
             <div>
                 <label for="sinopsis" class="formbold-form-label"> Sinopsis </label>
-                <textarea rows="4" name="sinopsis" placeholder="Tuliskan sinopsis.." class="formbold-form-input"></textarea>
+                <textarea rows="4" name="sinopsis" class="formbold-form-input" value="<?php echo $row['sinopsis'] ?>"></textarea>
             </div>
 
               <div class="formbold-input-flex">
                 <div>
                     <label for="cover_buku" class="formbold-form-label"> Cover Buku </label>
-                    <input type="file" name="cover_buku" class="formbold-form-input"/>
+                    <input type="file" name="cover_buku" class="formbold-form-input" value=<?php echo "<img src='../../images/" . $row['picture'] . "'>"; ?>/>
                 </div>
               </div>
       
-              <button class="formbold-btn">
-                  Tambah
-              </button>
+              <input type="submit" class="formbold-btn" name="up" value="Update"></input>
           </form>
+          <?php endwhile; ?>
         </div>
       </div>
 </body>
